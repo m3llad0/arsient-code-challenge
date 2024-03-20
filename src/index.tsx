c592caa9986821a109e761f5cdbdf316d102ca40
m3llad0 <a01655451@tec.mx>
2024-03-20 12:39:13 -0600

    implemented reacto router

--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './pages/App';
+import Nft from './pages/Nft';
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="*" element={<Navigate to="/" />} />
+        <Route path='/nft/:id' element = {<Nft/>}/>
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
