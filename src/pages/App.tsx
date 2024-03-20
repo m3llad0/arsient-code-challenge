ae5b44566e6e7c700257ad06014c71bef4a1c04e
m3llad0 <a01655451@tec.mx>
2024-03-20 12:38:38 -0600

    updated link redirection on click in images

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -52,7 +52,7 @@ export default function App() {
 
         <div className="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
           {products.map((product) => (
-            <a key={product.id} href={product.href} className="group">
+            <a key={product.id}  className="group" href={`nft/${product.id}`}>
               <figure className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-h-8 xl:aspect-w-7">
                 <img
                   src={product.imageSrc}
