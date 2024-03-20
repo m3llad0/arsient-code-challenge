7b90589f285efa49c173a033d3c03c8bd2807cb4
m3llad0 <a01655451@tec.mx>
2024-03-19 20:36:54 -0600

    price now in eth

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -62,7 +62,7 @@ export default function App() {
               </figure>
               <figcaption>
                 <h3 className="mt-4 text-sm text-gray-700">{product.name}</h3>
-                <p className="mt-1 text-lg font-medium text-gray-900">{product.price}</p>
+                <p className="mt-1 text-lg font-medium text-gray-900">{product.price} ETH</p>
               </figcaption>
             </a>
           ))}
